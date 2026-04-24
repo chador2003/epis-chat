2958b5ab2aa4dae534d289ceeb53d152a5654691
Chador <chadorhdr10plus@gmail.com>
2026-04-24 12:45:18 +0600

    api url updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:9000/chat";
+const API_URL = "https://epis.huddymerrabuddy2003.workers.dev/api/chat";
 const sendBtn = document.getElementById("sendBtn");
 const input = document.getElementById("user-input");
 const messages = document.getElementById("chat-messages");
